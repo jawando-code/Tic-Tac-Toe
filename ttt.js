a6e0299999909745d27c82a9ba6c064d80134c3c
jawando-code <peruvianswing@gmail.com>
2026-05-14 14:06:23 +0000

    Add rudimentary logic to click buttons

--- ttt.js
+++ ttt.js
@@ -1,25 +1,35 @@
  
 
 const beginGame = (() => {
-   
-const board = document.querySelector(".board")
-const gameboard = new Array(9).fill("_",0);
-   
-const btns = document.querySelectorAll(".btn");
-const markers = document.querySelector('.markers')
+    const markers = document.querySelector('.markers')
+    const btns = document.querySelectorAll(".btn");
+    const board = document.querySelector(".board")
+    const gameboard = new Array(9).fill("_",0);
+    let player1;
+    let player2;
+    btns.forEach((button)=> {
+            button.addEventListener('click', ()=> {
+                (() => {
+                markers.style.display = "none"
+                })()
+               
+                player1 = button.value;
 
-
-let player1;
-btns.forEach((button)=> {
-        button.addEventListener('click', ()=> {
-            (() => {
-            markers.style.display = "none"
-            })()
-            console.log(button.value + "Jackdaniels")
-            player1 = button.value;
-            console.log(player1)
+                (()=>{
+                    if(player1 === 'x'){
+                        player2 = 'o';
+                    } else if(player1 === "o"){
+                        player2 = "x";
+                    }
+                })()
+                
+                
+            })
         })
-    })
+    
+            
+     
+    
 
     gameboard.forEach((item)=> {
        
@@ -27,21 +37,49 @@ btns.forEach((button)=> {
         inputSquare.className = "input";
         inputSquare.textContent = '';
         board.append(inputSquare)
+       
         inputSquare.addEventListener('click', ()=> {
 
+            const inputs = document.querySelectorAll('.input')
             if(player1 === 'o' && inputSquare.textContent === "" ){
                 inputSquare.textContent = "o"
+                player2 = "x"
             } else if(player1 === 'x' && inputSquare.textContent === ""){
                 inputSquare.textContent = "x"
+                player2 = "o"
+                console.log("click")
             }
+            console.log("this is player 2" + player2)
         })
+        
     })
 
-
-
 })
+  let botMove = function (){
+             inputs[3].click();
+            if(player1 === 'o' && inputSquare.textContent === ''){
+                player2 = 'x'
+                inputSquare.textContent = 'x'
+               
+                console.log('human ' + player1, 'bot ' + player2);
+            }  else if(player1 === 'x' && inputSquare.textContent === ''){
+                player2 = 'o';
+                inputSquare.textContent = "o";
+                inputs[1].click();
+                console.log('human ' + player1, 'bot ' + player2);
+            }}  
+            
+const botResponse = () => {
+     const board = document.querySelector('.board');
+     const inputs = document.querySelectorAll('.input')
+    board.addEventListener("click", event => {
+     if(event.target !== event.currentTarget) {                
+      
+            
 
+            }
 
+})}
 function createPlayer(name){
 const playerName = name;
 
@@ -86,3 +124,21 @@ const newGame = (() => {
 })
 
 
+let clicker = function() {
+    const board = document.querySelector('.board');
+    const inputs = document.querySelectorAll(".input")
+    board.addEventListener("click",()=> {
+
+    inputs[0].click();
+    inputs[1].click();
+    inputs[4].click()
+    
+    console.log("Clicked")
+
+
+
+    })
+   
+}
+
+clicker();
